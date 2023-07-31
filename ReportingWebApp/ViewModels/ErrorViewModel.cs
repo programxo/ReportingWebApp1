4905353e4da71f36085f822d915181527a4cfa86
programxo <shamil.abubakr@proton.me>
2023-07-31 13:57:32 +0200

    Add project files.

---
+++ ReportingWebApp/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReportingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
