fd7d8ef4ec374557b396577a039290d8af3c323a
programxo <shamil.abubakr@proton.me>
2023-07-31 14:05:31 +0200

    2. Add Identity Model with User Roles

---
+++ ReportingWebApp/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ReportingWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public Role UserRole { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        Employee
+    }
+}
